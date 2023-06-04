188758435da508cce9a66e72e1d560c0c74ac2b7
chronoabi <abishekpoudel2014@gmail.com>
2023-06-04 22:53:57 +0545

    initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col gap-5 px-4" >
+    <h1 class="text-4xl font-bold underline text-primary text-center pt-8" >Type test</h1>
+    <button class="bg-primary font-semibold uppercase text-lg py-4 px-6" >Press me</button>
+</div>
